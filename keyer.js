97f8194b3ff477cca5290040c0dcbd2952af5351
vrchavezport <vrchavezport@gmail.com>
2016-08-08 17:50:17 -0700

    Create keyer.js

---
+++ keyer.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+			
+	keyer();
+			
+	$(window).resize(keyer);
+	
+})
+		
+function keyer() {
+	$(document).keydown(function(e){
+		if(e.keyCode==40 && $(window).scrollTop() < 400){	
+			e.preventDefault();
+			var target1= $("#beabout");
+			$("body, html").stop().animate({
+				scrollTop: target1.offset().top
+			}, 1000);
+		}
+
+		if(e.keyCode==40 && $(window).scrollTop() > 400 && $(window).scrollTop() < 1200){	
+			e.preventDefault();
+			var target2= $("#bework");
+			$("body, html").stop().animate({
+				scrollTop: target2.offset().top
+			}, 1000);
+		}
+
+		if(e.keyCode==38 && $(window).scrollTop() > 400 && $(window).scrollTop() < 1200){	
+			e.preventDefault();
+			var target3= $("#bename");
+			$("body, html").stop().animate({
+				scrollTop: target3.offset().top
+			}, 1000);
+		}				
+				
+		if(e.keyCode==40 && $(window).scrollTop() > 1200 && $(window).scrollTop() < 2400){	
+			e.preventDefault();
+			var target4= $("#beskills");
+			$("body, html").stop().animate({
+				scrollTop: target4.offset().top
+			}, 1000);				
+		}
+			
+		if(e.keyCode==38 && $(window).scrollTop() > 1200 && $(window).scrollTop() < 2400){	
+			e.preventDefault();
+			var target4= $("#beabout");
+			$("body, html").stop().animate({
+				scrollTop: target4.offset().top
+			}, 1000);				
+		}
+
+		if(e.keyCode==40 && $(window).scrollTop() > 2400 && $(window).scrollTop() < 3100){	
+			e.preventDefault();
+			var target5= $("#beschool");
+			$("body, html").stop().animate({
+				scrollTop: target5.offset().top
+			}, 1000);				
+		}
+
+		if(e.keyCode==38 && $(window).scrollTop() > 2400 && $(window).scrollTop() < 3100){	
+			e.preventDefault();
+			var target6= $("#bework");
+			$("body, html").stop().animate({
+				scrollTop: target6.offset().top
+			}, 1000);				
+		}
+
+		if(e.keyCode==38 && $(window).scrollTop() > 3100){	
+			e.preventDefault();
+			var target7= $("#beskills");
+			$("body, html").stop().animate({
+				scrollTop: target7.offset().top
+			}, 1000);				
+		}								
+	});
+};
