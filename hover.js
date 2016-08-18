77babad3af14d8e86614bd4c0ef5f2bb4f21028c
vrchavezport <vrchavezport@gmail.com>
2016-08-18 13:20:31 -0700

    Create hover.js

---
+++ hover.js
@@ -0,0 +1,141 @@
+		$("#nounder").hover(function(){
+			$(this).css("color", "#e9e9e9");
+		}, function(){
+			$(this).css("color", "black");
+		});
+		
+		$("#nounder2").hover(function(){
+			$(this).css("color", "#e9e9e9");
+		}, function(){
+			$(this).css("color", "black");
+		});
+		
+		$("#contlink").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#icons").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() < 400) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#icons').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#icons').css({"color" : "black"});
+			}
+		});
+		
+		$("#icons").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#contlink").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() < 400) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#contlink').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#contlink').css({"color" : "black"});
+			}
+		});
+		
+		$("#aboutlink").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#icons2").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 400 && $(window).scrollTop() < 1200) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#icons2').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#icons2').css({"color" : "black"});
+			}
+		});
+		
+		$("#icons2").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#aboutlink").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 400 && $(window).scrollTop() < 1200) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#aboutlink').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#aboutlink').css({"color" : "black"});
+			}
+		});
+		
+		$("#worklink").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#icons3").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 1600 && $(window).scrollTop() < 2400) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#icons3').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#icons3').css({"color" : "black"});
+			}
+		});
+		
+		$("#icons3").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#worklink").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 1200 && $(window).scrollTop() < 2400) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#worklink').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"})
+				$('#worklink').css({"color" : "black"});
+			}
+		});
+		
+		$("#skilltext").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#skillclick").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 2550 && $(window).scrollTop() < 2950) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#skillclick').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"});
+				$('#skillclick').css({"color" : "black"});
+			}
+		});
+		
+		$("#skillclick").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#skilltext").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 2550 && $(window).scrollTop() < 2950) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#skilltext').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"});
+				$('#skilltext').css({"color" : "black"});
+			}
+		});
+		
+		$("#schooltext").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#schoolclick").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 3100) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#schoolclick').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"});
+				$('#schoolclick').css({"color" : "black"});
+			}
+		});
+		
+		$("#schoolclick").hover(function(){
+			$(this).css("color", "#e9e9e9");
+			$("#schooltext").css("color", "#e9e9e9");
+		}, function(){
+			if($(window).scrollTop() > 3100) {
+				$(this).css({"color" : "#7EB7A1"})
+				$('#schooltext').css({"color" : "#7EB7A1"});
+			} else {
+				$(this).css({"color" : "black"});
+				$('#schooltext').css({"color" : "black"});
+			}
+		});	
