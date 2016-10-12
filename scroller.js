a330b0a7cc56f0ce13604c71be74c8105de74e1e
vrchavezport <vrchavezport@gmail.com>
2016-10-12 00:05:47 -0700

    Update scroller.js

--- scroller.js
+++ scroller.js
@@ -7,42 +7,42 @@ $(document).ready(function() {
 })		
 
 function scroller() {
-	if($(".top").css("position") == "fixed"){
-		$('#worklink, #icons3').on('click', function(e){
+	if($(".menu").css("height") == "50px"){
+		$('#abou').on('click', function(e){
 			e.preventDefault();
-			var target= $(this).get(0).href == 'bework' ? $('#bework') : $('#bework');
+			var target= $(this).get(0).href == 'aboutAnchor' ? $('#aboutAnchor') : $('#aboutAnchor');
 			$('html, body').stop().animate({
 				scrollTop: target.offset().top
 			}, 1000);
 		});
 				
-		$('#aboutlink, #icons2').on('click', function(e){
+		$('#ski').on('click', function(e){
 			e.preventDefault();
-			var targett= $(this).get(0).href == 'beabout' ? $('#beabout') : $('#beabout');
+			var targett= $(this).get(0).href == 'skillAnchor' ? $('#skillAnchor') : $('#skillAnchor');
 			$('html, body').stop().animate({
 				scrollTop: targett.offset().top
 			}, 1000);
 		});
 				
-		$('#contlink, #icons').on('click', function(e){
+		$('#wor').on('click', function(e){
 			e.preventDefault();
-			var targettt= $(this).get(0).href == 'bename' ? $('#bename') : $('#bename');
+			var targettt= $(this).get(0).href == 'workAnchor' ? $('#workAnchor') : $('#workAnchor');
 			$('html, body').stop().animate({
 				scrollTop: targettt.offset().top
 			}, 1000);
 		});
 				
-		$('#skilltext, #skillclick').on('click', function(e){
+		$('#schoo').on('click', function(e){
 			e.preventDefault();
-			var targetttt= $(this).get(0).href == 'beabout' ? $('#beskills') : $('#beskills');
+			var targetttt= $(this).get(0).href == 'schoolAnchor' ? $('#schoolAnchor') : $('#schoolAnchor');
 			$('html, body').stop().animate({
 				scrollTop: targetttt.offset().top
 			}, 1000);
 		});
 				
-		$('#schooltext, #schoolclick').on('click', function(e){
+		$('#conta').on('click', function(e){
 			e.preventDefault();
-			var targettttt= $(this).get(0).href == 'beschool' ? $('#beschool') : $('#beschool');
+			var targettttt= $(this).get(0).href == 'contactAnchor' ? $('#contactAnchor') : $('#contactAnchor');
 			$('html, body').stop().animate({
 				scrollTop: targettttt.offset().top
 			}, 1000);
