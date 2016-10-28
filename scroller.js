0a135f177749413e0266738c2c5388a9f37c7914
vrchavezport <vrchavezport@gmail.com>
2016-10-27 17:46:16 -0700

    Update scroller.js

--- scroller.js
+++ scroller.js
@@ -10,41 +10,36 @@ function scroller() {
 	if($(".menu").css("height") == "50px"){
 		$('#abou').on('click', function(e){
 			e.preventDefault();
-			var target= $(this).get(0).href == 'aboutAnchor' ? $('#aboutAnchor') : $('#aboutAnchor');
 			$('html, body').stop().animate({
-				scrollTop: target.offset().top
+				scrollTop: $('#aboutAnchor').offset().top
 			}, 1000);
 		});
 				
 		$('#ski').on('click', function(e){
 			e.preventDefault();
-			var targett= $(this).get(0).href == 'skillAnchor' ? $('#skillAnchor') : $('#skillAnchor');
 			$('html, body').stop().animate({
-				scrollTop: targett.offset().top
+				scrollTop: $('#skillAnchor').offset().top
 			}, 1000);
 		});
 				
 		$('#wor').on('click', function(e){
 			e.preventDefault();
-			var targettt= $(this).get(0).href == 'workAnchor' ? $('#workAnchor') : $('#workAnchor');
 			$('html, body').stop().animate({
-				scrollTop: targettt.offset().top
+				scrollTop: $('#workAnchor').offset().top
 			}, 1000);
 		});
 				
 		$('#schoo').on('click', function(e){
 			e.preventDefault();
-			var targetttt= $(this).get(0).href == 'schoolAnchor' ? $('#schoolAnchor') : $('#schoolAnchor');
 			$('html, body').stop().animate({
-				scrollTop: targetttt.offset().top
+				scrollTop: $('#schoolAnchor').offset().top
 			}, 1000);
 		});
 				
 		$('#conta').on('click', function(e){
 			e.preventDefault();
-			var targettttt= $(this).get(0).href == 'contactAnchor' ? $('#contactAnchor') : $('#contactAnchor');
 			$('html, body').stop().animate({
-				scrollTop: targettttt.offset().top
+				scrollTop: $('#contactAnchor').offset().top
 			}, 1000);
 		});				
 	}
