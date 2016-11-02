329dbf15ec9ebb78cb788213d4cdbcb6f7d06977
vrchavezport <vrchavezport@gmail.com>
2016-11-01 22:11:03 -0700

    Update checkplace.js

--- checkplace.js
+++ checkplace.js
@@ -9,6 +9,14 @@ function checkplace() {
 	$(window).bind('scroll', function() {
 		if(($(window).scrollTop() > 300) &&($(window).scrollTop() < 810)) {
 			$('#abou').css({"border" : "solid 2px #141823"})
+		} else if(($(window).scrollTop() > 825) &&($(window).scrollTop() < 1523)) {
+			$('#ski').css({"border" : "solid 2px #141823"})
+		} else if(($(window).scrollTop() > 1580) &&($(window).scrollTop() < 2700)) {
+			$('#wor').css({"border" : "solid 2px #141823"})
+		} else if(($(window).scrollTop() > 2770) &&($(window).scrollTop() < 3300)) {
+			$('#schoo').css({"border" : "solid 2px #141823"})
+		} else if($(window).scrollTop() > 3300) {
+			$('#conta').css({"border" : "solid 2px #141823"})
 		} else {
 			$('#abou').css({"border" : "solid 2px #f8f8f8"});
 			$('#abou').mouseover(function(){
@@ -17,38 +25,6 @@ function checkplace() {
 			$('#abou').mouseout(function(){
 			    $('#abou').css("border", "solid 2px #f8f8f8");
 			});
-		}
-	});
-			
-	$(window).bind('scroll', function() {
-		if(($(window).scrollTop() > 825) &&($(window).scrollTop() < 1523)) {
-			$('#ski').css({"border" : "solid 2px #141823"})
-		} else {
-			$('#ski').css({"border" : "solid 2px #f8f8f8"});
-		}
-	});
-			
-	$(window).bind('scroll', function() {
-		if(($(window).scrollTop() > 1580) &&($(window).scrollTop() < 2700)) {
-			$('#wor').css({"border" : "solid 2px #141823"})
-		} else {
-			$('#wor').css({"border" : "solid 2px #f8f8f8"});
-		}
-	});
-			
-	$(window).bind('scroll', function() {
-		if(($(window).scrollTop() > 2770) &&($(window).scrollTop() < 3300)) {
-			$('#schoo').css({"border" : "solid 2px #141823"})
-		} else {
-			$('#schoo').css({"border" : "solid 2px #f8f8f8"});
-		}
-	});
-			
-	$(window).bind('scroll', function() {
-		if($(window).scrollTop() > 3300) {
-			$('#conta').css({"border" : "solid 2px #141823"})
-		} else {
-			$('#conta').css({"border" : "solid 2px #f8f8f8"});
 		}
 	});
 }
